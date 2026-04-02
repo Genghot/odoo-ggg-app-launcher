3a3dd37a6a6336d7409e921878639ff97dfc10cc
Genghot <kenghot@gmail.com>
2026-04-02 13:37:11 +0700

    init: ggg_app_launcher marketplace module scaffold

---
+++ addons/ggg_app_launcher/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'GGG App Launcher',
+    'version': '19.0.1.0.0',
+    'category': 'Productivity',
+    'summary': 'Quick app launcher for fast navigation between Odoo modules',
+    'description': '',
+    'author': 'GGG',
+    'website': 'https://ggg.com',
+    'license': 'LGPL-3',
+    'price': 0,
+    'currency': 'EUR',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'assets': {},
+    'images': [
+        'static/description/banner.png',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
